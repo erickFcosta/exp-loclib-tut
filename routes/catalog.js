45f69b12a0d4ee1577c5b1d714988e8cc2fd0dde
Camila Costa <erickcosta@pop-os.localdomain>
2022-03-09 00:23:18 -0300

    090023032022

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,13 +7,55 @@ let authorCtrl = require('../controllers/authorController');
 let bookInsCtrl = require('../controllers/bookinstanceController');
 let genreCtrl = require("../controllers/genreController");
 
-// TODO: book routers
+// book main routers
+router.get('/', bookCtrl.index);
+router.get('/book', bookCtrl.book_list);
+router.get('/book/:id', bookCtrl.book_detail);
+// get and post book
+router.get('/book/create', bookCtrl.book_create_get);
+router.post('/book/create', bookCtrl.book_create_post);
 
-// TODO: author routers
+router.get('/book/:id/update/', bookCtrl.book_update_get);
+router.post('/book/:id/update/', bookCtrl.book_update_post);
 
-// TODO: book instance routers
+router.get('/book/:id/delete/', bookCtrl.book_delete_get);
+router.post('/book/:id/delete/', bookCtrl.book_delete_post);
 
-// TODO: genre routers
+// author main routers
+router.get('/author', authorCtrl.author_list);
+router.get('/author/:id', authorCtrl.author_detail);
+// get and post author
+router.get('/author/create', authorCtrl.author_create_get);
+router.post('/author/create', authorCtrl.author_create_post);
+
+router.get('/author/:id/update', authorCtrl.author_update_get);
+router.post('/author/:id/update', authorCtrl.author_update_post);
+
+router.get('/author/:id/delete', authorCtrl.author_delete_get);
+router.post('/author/:id/delete', authorCtrl.author_delete_post);
+
+// book instance main routers
+router.get('/bookinstance', bookInsCtrl.bookinstance_list);
+router.post('/bookinstance/:id', bookInsCtrl.bookinstance_detail);
+// get and post book instance
+router.get('/bookinstance/:id/create', bookInsCtrl.bookinstance_create_get);
+router.post('/bookinstance/:id/create', bookInsCtrl.bookinstance_create_post);
+
+router.get('/bookinstance/:id/update', bookInsCtrl.bookinstance_update_get);
+router.post('/bookinstance/:id/update', bookInsCtrl.bookinstance_update_post);
+
+router.get('/bookinstance/:id/delete', bookInsCtrl.bookinstance_delete_get);
+router.post('/bookinstance/:id/delete', bookInsCtrl.bookinstance_delete_post);
+
+// genre main routers
+router.get('/genre', genreCtrl.genre_list);
+router.post('/genre/:id', genreCtrl.genre_detail);
+// get and post genre
+router.get('/genre/:id/update', genreCtrl.genre_update_get);
+router.post('/genre/:id/update', genreCtrl.genre_update_post);
+
+router.get('/genre/:id/delete', genreCtrl.genre_delete_get);
+router.post('/genre/:id/delete', genreCtrl.genre_delete_post);
 
 // module exports
 module.exports = router;
